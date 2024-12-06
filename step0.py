b457c7a68ca1b8436f7d3c2565f3da200cd72006
devinisaperson <devinisaperson@users.noreply.github.com>
2024-12-05 22:15:43 -0500

    initial upload
    contains files going step-by-step from creating a pygame window to basic character control to collision

---
+++ step0.py
@@ -0,0 +1,13 @@
+import pygame
+
+# init MUST be included before pygame can do anything
+pygame.init()
+
+# creates a window that's 1280 pixels wide and 720 pixels tall
+screen = pygame.display.set_mode((1280, 720)) 
+
+# while True keeps running forever
+while True:
+    # checks what is happening to the window (keyboard, clicking edges, other stuff you won't use)
+    # your computer may think the window crashed if you don't check this
+    pygame.event.get()
